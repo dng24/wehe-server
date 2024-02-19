85062e728fdf49d6b867d55ef63f19e6b427060a
Derek Ng <37806620+dng24@users.noreply.github.com>
2024-02-19 18:40:41 -0500

    add packet capture feature

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module wehe-server
 go 1.21.5
 
 require (
+	github.com/google/gopacket v1.1.19
 	github.com/shirou/gopsutil/v3 v3.24.1
 	gonum.org/v1/gonum v0.14.0
 	gopkg.in/ini.v1 v1.67.0
@@ -17,5 +18,6 @@ require (
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 )
---
+++ internal/network/packetcapture.go
@@ -0,0 +1,75 @@
+// Does packet captures.
+package network
+
+import (
+    "os"
+    "path/filepath"
+
+    "github.com/google/gopacket"
+    "github.com/google/gopacket/layers"
+    "github.com/google/gopacket/pcapgo"
+)
+
+type PacketCapture struct {
+    iface string // the interface to listen to
+    handle *pcapgo.EthernetHandle // the socket to capture packets
+    packets []gopacket.Packet // list of packets captured
+}
+
+func NewPacketCapture(iface string) (*PacketCapture, error) {
+    handle, err := pcapgo.NewEthernetHandle(iface)
+    if err != nil {
+        return nil, err
+    }
+    return &PacketCapture{
+        iface: iface,
+        handle: handle,
+    }, nil
+}
+
+// Captures packets. This function should be run in a new thread, as it does not return until
+// StopPacketCapture is called.
+func (packetCapture *PacketCapture) StartPacketCapture() {
+    packetSrc := gopacket.NewPacketSource(packetCapture.handle, layers.LayerTypeEthernet)
+    // capture packets
+    for packet := range packetSrc.Packets() {
+        packetCapture.packets = append(packetCapture.packets, packet)
+    }
+}
+
+// Stops capturing packets.
+func (packetCapture *PacketCapture) StopPacketCapture() {
+    packetCapture.handle.Close()
+}
+
+// Write captured packets to PCAP file.
+// filename: the output PCAP filename that the packets should be written to
+// Returns any errors
+func (packetCapture *PacketCapture) WriteToPcap(filename string) error {
+    // make sure all directories of output path exists
+    dir := filepath.Dir(filename)
+    err := os.MkdirAll(dir, os.ModePerm)
+    if err != nil {
+        return err
+    }
+
+    file, err := os.Create(filename)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    pcapWriter := pcapgo.NewWriter(file)
+    err = pcapWriter.WriteFileHeader(1600, layers.LinkTypeEthernet)
+    if err != nil {
+        return err
+    }
+    // write packets to file
+    for _, packet := range packetCapture.packets {
+        err = pcapWriter.WritePacket(packet.Metadata().CaptureInfo, packet.Data())
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+}
